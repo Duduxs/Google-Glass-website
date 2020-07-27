3950d9a99b59689c6d5b364d2bbedc8b04602388
Duduxs <duduxss3@gmail.com>
2020-07-27 17:48:16 -0300

    WebSite Finished

--- _project/_javascript/functions.js
+++ _project/_javascript/functions.js
@@ -3,3 +3,11 @@ function changePicture(picture){
     document.getElementById('glass').src = picture;
 
 }
+
+/*about*/
+function calc_total(){
+    const qtd = Number(document.getElementById('tNumber').value)
+    let tot = qtd * 1500;
+    document.getElementById('txtTotalPrice').value = tot;
+} 
+/*about*/
